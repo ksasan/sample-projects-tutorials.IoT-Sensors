952ebb81b46d453dd1112e753d738635ad95565e
Khelender Sasan <khelender.sasan@gmail.com>
2020-11-01 22:31:49 +0530

    Env file that possibly made things worked
     On branch master
     Changes to be committed:
    	new file:   env.sh

---
+++ env.sh
@@ -0,0 +1,11 @@
+export DEBUG=tutorial:*
+export WEB_APP_PORT=3000
+export IOTA_HTTP_HOST=iot-agent
+export IOTA_HTTP_PORT=7896
+export DUMMY_DEVICES_PORT=3001
+export DUMMY_DEVICES_API_KEY=4jggokgpepnvsb2uv4s40d59ov
+export IOTA_SOUTH_PORT=3002
+export MONGO_DB_VERSION=3.6
+export MONGO_DB_PORT=27017
+export ORION_VESION=latest
+export ORION_PORT=1026
